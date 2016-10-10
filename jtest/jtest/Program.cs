d384a45577da80a5c0f7f720d923e3bee2ff6e61
zeke <zhengq@shinetechchina.com>
2016-10-10 14:29:19 +0800

    set error

--- jtest/jtest/Program.cs
+++ jtest/jtest/Program.cs
@@ -10,7 +10,7 @@ namespace jtest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("jenkins build test");
+            Console.WriteLine("jenkins build test");u
         }
     }
 }
