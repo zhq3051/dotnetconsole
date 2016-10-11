ec624c39f86e590080851e2aaba7d80b38c0fd66
zeke <zhengq@shinetechchina.com>
2016-10-11 11:12:29 +0800

    roll back -1

--- jtest/jtest/Program.cs
+++ jtest/jtest/Program.cs
@@ -10,7 +10,7 @@ namespace jtest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("jenkins build test");u
+            Console.WriteLine("jenkins build test");
         }
     }
 }
